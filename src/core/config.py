e6839aab653004223af566a7475fac25f9556156
Ilya Filippov <i.filippov@idaproject.com>
2022-10-08 01:01:07 +0700

    feat(add_docker_file) add docker container

--- src/core/config.py
+++ src/core/config.py
@@ -23,6 +23,7 @@ class PostgresConfig(EnvBaseSettings):
     class Config:
         env_prefix = "db_"
 
+
 class Settings(EnvBaseSettings):
     http_server_host: str = "localhost"
     http_server_port: int = 8000
@@ -31,29 +32,5 @@ class Settings(EnvBaseSettings):
     reload: bool = False
     db: PostgresConfig = PostgresConfig()
 
+
 settings = Settings()
-
-
-# class Settings(object):
-#     # Server
-#     HTTP_SERVER_HOST = os.environ.get("HTTP_SERVER_HOST")
-#     HTTP_SERVER_PORT = int(os.environ.get("HTTP_SERVER_PORT"))
-#     COUNT_WORKERS_UVICORN = int(os.environ.get("COUNT_WORKERS_UVICORN", 1))
-#
-#     # PostgreSQL
-#     DB_HOST = os.environ.get("DB_HOST")
-#     DB_PORT = int(os.environ.get("DB_PORT"))
-#     DB_USER = os.environ.get("DB_USER")
-#     DB_PASSWORD = os.environ.get("DB_PASSWORD")
-#     DATABASE = os.environ.get("DATABASE")
-#
-#     DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DATABASE}"
-#
-#     # На данный момент отключил за ненадобностью, так как получилось перевести часть логики на запрос к БД.
-#     # Redis
-#     # REDIS_HOST = f"""redis://{os.environ.get("REDIS_HOST")}"""
-#     # REDIS_PORT = int(os.environ.get("REDIS_PORT"))
-#     # REDIS_ZONE_INFO_DB = int(os.environ.get("REDIS_ZONE_INFO_DB"))
-# #
-#     # Redis cache
-#     # CACHE_ZONE_KEY = os.environ.get("CACHE_ZONE_KEY")
